5e01aef5c54847649ed87aa17fcc20f925d70794
Slikek <73469414+Av10n@users.noreply.github.com>
2023-01-05 09:34:47 +0300

    changes...

---
+++ Player.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+const moveSpeed = 200
+var vel = Vector2()
+var sprint = false
+
+func _physics_process(delta):
+	vel = Vector2.ZERO
+	
+	if Input.is_action_pressed("player_right"):
+		vel.x = moveSpeed
+	if Input.is_action_pressed("player_left"):
+		vel.x = -moveSpeed
+	if Input.is_action_pressed("player_up"):
+		vel.y = -moveSpeed
+	if Input.is_action_pressed("player_down"):
+		vel.y = moveSpeed
+	
+	if Input.is_action_just_pressed("player_sprint"):
+		sprint = true
+	if Input.is_action_just_released("player_sprint"):
+		sprint = false
+	
+
+	if sprint:
+		move_and_slide(vel * 2)
+	else:
+		move_and_slide(vel)
