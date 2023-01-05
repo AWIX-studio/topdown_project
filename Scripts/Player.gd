1a583e7bdb525f7b8a5c79736acb850b10a83c68
Slikek <73469414+Av10n@users.noreply.github.com>
2023-01-05 17:04:05 +0300

    player animations

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,20 +6,36 @@ var sprint = false
 
 func _physics_process(delta):
 	vel = Vector2.ZERO
-	
+#	if Input.is_action_just_pressed("player_right") or Input.is_action_just_pressed("player_left") or Input.is_action_just_pressed("player_up") or Input.is_action_just_pressed("player_down"):
+#		$AnimationPlayer.stop()
+		
 	if Input.is_action_pressed("player_right"):
 		vel.x = moveSpeed
-	if Input.is_action_pressed("player_left"):
+		
+	elif Input.is_action_pressed("player_left"):
 		vel.x = -moveSpeed
+		
 	if Input.is_action_pressed("player_up"):
 		vel.y = -moveSpeed
-	if Input.is_action_pressed("player_down"):
+		
+	elif Input.is_action_pressed("player_down"):
 		vel.y = moveSpeed
+	
+	if Input.is_action_pressed("player_down") or Input.is_action_pressed("player_left") or Input.is_action_pressed("player_right") or Input.is_action_pressed("player_up"):
+		if sprint:
+			$AnimationPlayer.play("player_running")
+		else:
+			$AnimationPlayer.play("player_walking")
+	else:
+		$AnimationPlayer.play("player_idle")
+		
 	
 	if Input.is_action_just_pressed("player_sprint"):
 		sprint = true
+		
 	if Input.is_action_just_released("player_sprint"):
 		sprint = false
+	
 	
 
 	if sprint:
